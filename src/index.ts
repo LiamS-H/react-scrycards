d92700cdbcf8ccf71a99ce495726c7fb300ad7bf
LiamS-H <liamsh@gmail.com>
2024-09-20 02:43:03 -0700

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export { useScrycard } from "./hooks/useScrycard";
 export { default as Scrycard } from "./components/Scrycard";
+export { default as Scrytext } from "./components/Scrytext";
+export { default as ScryNameCard } from "./components/ScryNameCard";
 export {
     ScrycardsContextProvider,
     useScrycardsContext,
 } from "./contexts/scrycards";
-export type * from "./types/scryfallcards";
+export type * from "./types/scryfall/cards";
+export type * from "./types/scryfall/symbols";
